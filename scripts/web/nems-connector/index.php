31bdce8d958156df9e15225d9bf76ecafc37d216
Your Name <you@example.com>
2020-09-19 17:13:51 -0400

    Begin work on nems-connector

---
+++ scripts/web/nems-connector/index.php
@@ -0,0 +1,14 @@
+<?php
+  $data = array();
+  $data['version'] = '1.0';
+  if (file_exists('/var/www/html/apps/')) {
+    $data['activated'] = TRUE;
+  } else {
+    $data['activated'] = FALSE;
+  }
+  if (file_exists('/var/log/JMICRON_RAID_CONTROLLER.log')) {
+    $data['raid_status']=trim(shell_exec("grep RaidStatus /var/log/JMICRON_RAID_CONTROLLER.log | awk '{ print $" . "NF }'"));
+  }
+  echo json_encode($data);
+?>
+
