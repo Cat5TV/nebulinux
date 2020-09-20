21003aebd308eecef7642be3c0389b609f5dd8e7
Robbie Ferguson <robbie@category5.tv>
2020-09-19 20:29:12 -0400

    Error handling on first boot

--- scripts/web/nems-connector/index.php
+++ scripts/web/nems-connector/index.php
@@ -9,6 +9,9 @@
   if (file_exists('/var/log/JMICRON_RAID_CONTROLLER.log')) {
     $data['raid_status']=trim(shell_exec("grep RaidStatus /var/log/JMICRON_RAID_CONTROLLER.log | awk '{ print $" . "NF }'"));
   }
+  if (!isset($data['raid_status']) || strlen(trim($data['raid_status'])) == 0) {
+    $data['raid_status'] = 'Unknown';
+  }
   echo json_encode($data);
 ?>
 
